8b68feba12644b09c0b750f6fd31c8e031902143
Ashraf Emad <ashrafemad23@gmail.com>
2024-11-19 13:01:13 +0200

    add docs - add hint in sql file

--- python_django/api/urls.py
+++ python_django/api/urls.py
@@ -1,7 +1,23 @@
+from django.conf import settings
+from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import routers
 
 from api.views import FileUploaderAPI
 
-router = routers.DefaultRouter()
+schema_view = get_schema_view(
+    openapi.Info(
+        title="File Uploader API Docs",
+        default_version="v1",
+    ),
+    public=settings.DEBUG,
+)
+
+
+router = routers.SimpleRouter()
 router.register(r"file-uploads", FileUploaderAPI, basename="file-uploads")
 urlpatterns = router.urls
+urlpatterns += (
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+)
--- sql/task_queries/1_retrieve_by_course_name.sql
+++ sql/task_queries/1_retrieve_by_course_name.sql
@@ -8,6 +8,6 @@ FROM
     JOIN student_course USING(course_id)
     JOIN student USING(student_id)
 WHERE
-    course.title = 'History of Art'
+    course.title = 'Physics 101' -- to be changed with the required course
 ORDER BY
     student.student_id;
